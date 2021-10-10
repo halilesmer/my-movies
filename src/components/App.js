b7742cb198cd76d8a3af5fc6ade14771f69a7bb9
halilesmer <taximinator@gmail.com>
2021-10-10 18:42:14 +0200

    sort movies as last adding

--- src/components/App.js
+++ src/components/App.js
@@ -2,46 +2,90 @@ import React from "react";
 import MovieList from "./MovieList";
 import SearchBar from "./SearchBar";
 import AddMovie from "./AddMovie";
-
+import axios from "axios";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class App extends React.Component {
   state = {
     movies: [],
     searchQuery: "",
   };
-  async componentDidMount()  {
-    const baseUrl = "http://localhost:3002/movies";
-    const response = await fetch(baseUrl);
-    const data = await response.json();
-    console.log(data)
-    this.setState({ movies: data })
 
+  //Axios package
+  async componentDidMount() {
+    const response = await axios.get("http://localhost:3002/movies");
+    try {
+      this.setState({ movies: response.data });
+    } catch (error) {
+      console.error(error);
+    }
   }
+  //Delete Movie
+  deleteMovie = async (movie) => {
+    await axios.delete(`http://localhost:3002/movies/${movie.id}`);
 
-
-  deleteMovie = (movie) => {
     const deletedMovieList = this.state.movies.filter((m) => m.id !== movie.id);
     this.setState({ movies: deletedMovieList });
   };
+  // Search Movie
   searchMovie = (event) => {
     this.setState({ searchQuery: event.currentTarget.value.toLowerCase() });
   };
-
+  // Add Movie
+  addMovie = async (movie) => {
+    await axios.post(`http://localhost:3002/movies/`, movie);
+    this.setState((state) => ({ movies: state.movies.concat([movie]) }));
+  };
   render() {
-    const filteredMovies = this.state.movies.filter((movie) => {
-      return movie.name.toLowerCase().includes(this.state.searchQuery);
-    });
-    //console.log(filteredMovies);
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <SearchBar searchMovieProp={this.searchMovie} />
-          </div>
-        </div>
+    const filteredMovies = this.state.movies
+      .filter((movie) => {
+       
+        return movie.name
+          .toLowerCase()
+          .includes(this.state.searchQuery.toLowerCase());
+      })
+      .sort((a, b) => {
+        return a.id - b.id;
+      }).reverse();
+        
+    
 
-        <MovieList movies={filteredMovies} deleteMovieProp={this.deleteMovie} />
-      </div>
+    return (
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={() => (
+                <React.Fragment>
+                  <div className="row">
+                    <div className="col-lg-12">
+                      <SearchBar searchMovieProp={this.searchMovie} />
+                    </div>
+                  </div>
+
+                  <MovieList
+                    movies={filteredMovies}
+                    deleteMovieProp={this.deleteMovie}
+                  />
+                </React.Fragment>
+              )}
+            ></Route>
+
+            <Route
+              path="/"
+              render={() => (
+                <AddMovie
+                  onAddMovie={(movie) => {
+                    this.addMovie(movie);
+                  }}
+                />
+              )}
+            ></Route>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,31 +1,58 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 class SearchBar extends React.Component {
-
- /*  state = {
+  /*  state = {
     searchText: "",
   }; */
-    
-    handleSubmit = (event) => {
-      event.preventDefault()
-  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  };
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleFormSubmit}>
         <div className="form-row mb-5">
-          <div className="col-12 mt-2">
+          <div className="col-10">
             <input
               onChange={this.props.searchMovieProp}
               type="text"
               className="form-control"
-              placeholder="Search a movie"
-              onFocus={(e) => e.currentTarget.select()}
+              placeholder="Seach a movie"
             />
           </div>
+          <div className="col-2">
+            <Link
+              to="/add"
+              type="button"
+              className="btn btn-md btn-danger"
+              style={{ float: "right" }}
+            >
+              Add New Movie
+            </Link>
+          </div>
         </div>
-        SearchBar
       </form>
     );
   }
 }
 export default SearchBar;
+/*  <div className="col-2">
+   <button
+     type="button"
+     className="btn btn-md btn-danger"
+     style={{ float: "right" }}
+   >
+     Add Movie
+   </button>
+ </div>; */
+/* <Link
+              to="/add"
+              type="button"
+              className="btn btn-md btn-danger"
+              style={{ float: "right" }}
+            >
+              Add Movie
+            </Link>
+             */
